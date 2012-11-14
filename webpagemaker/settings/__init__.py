8dc606385555df2a4bdeda018b196dcea589f4c3
Atul Varma <varmaa@gmail.com>
2012-11-14 10:54:16 +0000

    If PORT is in os.environ, force heroku/12-factor.
    This makes it easier to switch between running from settings/local.py
    and foreman.

--- webpagemaker/settings/__init__.py
+++ webpagemaker/settings/__init__.py
@@ -1,6 +1,12 @@
+import os
+
 from .base import *
-try:
-    from .local import *
-except ImportError, exc:
-    print "settings/local.py not found, assuming 12-factor/heroku."
+if 'PORT' in os.environ:
+    print "PORT environment variable found, assuming 12-factor/heroku."
     from .heroku import *
+else:
+    try:
+        from .local import *
+    except ImportError, exc:
+        print "settings/local.py not found, assuming 12-factor/heroku."
+        from .heroku import *
