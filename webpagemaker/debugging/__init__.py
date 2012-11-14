d40cb5c38575a05bcec121e3cb10f5b8c9e10eb5
Atul Varma <varmaa@gmail.com>
2012-11-14 11:48:31 +0000

    added /throw endpoint for debug info (tracebacks, etc).

--- webpagemaker/debugging/__init__.py
+++ webpagemaker/debugging/__init__.py
@@ -2,6 +2,15 @@ from . import git
 
 from django.conf.urls.defaults import patterns, include
 
+def throw(request):
+    """
+    Throw an exception so that the user can see all of Django's
+    debugging information.
+    """
+
+    i_am_not_defined
+
 urlpatterns = patterns('',
+    (r'^throw$', throw),
     (r'^git-pull$', git.pull),
 )
