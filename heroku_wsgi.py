6587fcdea401a10bf6cec8ed6d93a9e99bda57a9
Atul Varma <varmaa@gmail.com>
2012-11-14 13:50:15 +0000

    attempt to redirect user to proper protocol if SITE_URL is defined.

--- heroku_wsgi.py
+++ heroku_wsgi.py
@@ -1,4 +1,6 @@
 import os
+from urlparse import urlparse
+from urllib import quote
 #import site
 
 os.environ.setdefault('CELERY_LOADER', 'django')
@@ -15,11 +17,27 @@ import manage
 import django.core.handlers.wsgi
 django_app = django.core.handlers.wsgi.WSGIHandler()
 
+SITE_SCHEME = urlparse(os.environ.get('SITE_URL', '')).scheme
+
 def application(environ, start_response):
     # For some reason 'secure_scheme_headers' in gunicorn config
     # is failing us, so we'll do it manually here.
     if environ.get('HTTP_X_FORWARDED_PROTO') == 'https':
         environ['wsgi.url_scheme'] = 'https'
+
+    # If the SITE_URL is explicitly defined, then we're going to
+    # force a redirect to the proper protocol (http or https)
+    # if necessary.
+    if SITE_SCHEME and environ['wsgi.url_scheme'] != SITE_SCHEME:
+        url = os.environ.get('SITE_URL')
+        # http://www.python.org/dev/peps/pep-0333/#url-reconstruction
+        url += quote(environ.get('SCRIPT_NAME', ''))
+        url += quote(environ.get('PATH_INFO', ''))
+        if environ.get('QUERY_STRING'):
+            url += '?' + environ['QUERY_STRING']
+        start_response("302 Found", [("Location", url)])
+        return ["redirect to %s" % SITE_SCHEME]
+
     return django_app(environ, start_response)
 
 # vim: ft=python
